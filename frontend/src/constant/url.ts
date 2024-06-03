0707308f620e7c8630b43388ece87679b0a4946e
Md Akhtar Uzzaman <akhtaruzzaman05@gmail.com>
2024-06-03 23:02:28 +0600

    treafik implementation on going

--- frontend/src/constant/url.ts
+++ frontend/src/constant/url.ts
@@ -1,7 +1,7 @@
-const baseApiUrl = 'https://localhost:9081/';
+const baseApiUrl = 'http://localhost:5180/';
 
 export const ApiUrl = {
     baseUrl: baseApiUrl,
 	  weatherApi: baseApiUrl + 'api/emailsender/api/WeatherForecast',
 	// Add other configuration values here as needed
-  };
+  }; 
